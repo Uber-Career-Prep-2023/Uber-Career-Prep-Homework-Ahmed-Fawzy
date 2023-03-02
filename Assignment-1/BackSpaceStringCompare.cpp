1f8a0d971c9894cc115c1055acb109a9085f3c01
Ahmed Fawzy <ahmed_ehab03@outlook.com>
2023-03-02 17:27:30 -0500

    code quality fix

--- Assignment-1/BackSpaceStringCompare.cpp
+++ Assignment-1/BackSpaceStringCompare.cpp
@@ -1,61 +1,73 @@
-//One pointer technique used to find string after all deletions
-//Space is O(1), runtime is O(n)  [n is the length of the longer input string]
+// One pointer technique used to find string after all deletions
+// Space is O(1), runtime is O(n)  [n is the length of the longer input string]
 
-/*Explanation: 
+/*Explanation:
 Loop through the characters if you see "#" delete the char before and current char (#)
 Compare both strings to see if they are equal
 */
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <bits/stdc++.h>
 
+bool BackSpaceStringCompare(std::string firstString, std::string secondString)
+{
 
+  for (int i = 0; i < firstString.length(); i++)
+  {
+    if (i == 0 && firstString[0] == '#')
+    {
+      firstString.erase(0, 1);
+      i--;
+    }
 
-bool BackSpaceStringCompare(std::string in1, std::string in2){
-
-for(int i=0;i<in1.length();i++){
-if(i==0 && in1[0]=='#') 
-  {in1.erase(0, 1);
-  i--;
+    if (i > 0 && firstString[i] == '#')
+    {
+      firstString.erase(i, 1);
+      firstString.erase(i - 1, 1);
+      i = i - 2;
+    }
   }
 
-if(i>0 && in1[i]=='#'){
-in1.erase(i, 1);
-in1.erase(i-1, 1);
-i=i-2;
-}
-}
+  for (int i = 0; i < secondString.length(); i++)
+  {
+    if (i == 0 && secondString[0] == '#')
+    {
+      secondString.erase(0, 1);
+      i--;
+    }
 
-for(int i=0;i<in2.length();i++){
-if(i==0 && in2[0]=='#') 
-  {in2.erase(0, 1);
-  i--;
+    if (i > 0 && secondString[i] == '#')
+    {
+      secondString.erase(i, 1);
+      secondString.erase(i - 1, 1);
+      i = i - 2;
+    }
   }
-
-if(i>0 && in2[i]=='#'){
-in2.erase(i, 1);
-in2.erase(i-1, 1);
-i=i-2;
-}
-}
-return(in1==in2);
+  return (firstString == secondString);
 }
 
+int main()
+{
 
-int main(){
+  std::string test1[2] = {"abcde", "abcde"};
+  std::string test2[2] = {"Uber Career Prep", "u#Uber Careee#r Prep"};
+  std::string test3[2] = {"abcdef###xyz", "abcw#xyz"};
+  std::string test4[2] = {"abcdef###xyz", "abcdefxyz###"};
 
-std::string test1[2]={"abcde","abcde"};
-std::string test2[2]={"Uber Career Prep","u#Uber Careee#r Prep"};
-std::string test3[2]={"abcdef###xyz","abcw#xyz"};
-std::string test4[2]={"abcdef###xyz","abcdefxyz###"};
-
-std::cout << std::boolalpha;
-std::cout<<"Input strings 1: \"abcde\", \"abcde\" "<<std::endl<<"Output: "<<BackSpaceStringCompare(test1[0],test1[1])<<std::endl<<std::endl;
-std::cout<<"Input strings 2: \"Uber Career Prep\", \"u#Uber Careee#r Prep\" "<<std::endl<<"Output: "<<BackSpaceStringCompare(test2[0],test2[1])<<std::endl<<std::endl;
-std::cout<<"Input strings 3: \"abcdef###xyz\", \"abcw#xyz\" "<<std::endl<<"Output: "<<BackSpaceStringCompare(test3[0],test3[1])<<std::endl<<std::endl;
-std::cout<<"Input strings 4: \"abcdef###xyz\",\"abcdefxyz###\" "<<std::endl<<"Output: "<<BackSpaceStringCompare(test4[0],test4[1])<<std::endl<<std::endl;
-
+  std::cout << std::boolalpha;
+  std::cout << "Input strings 1 with no deletes: \"abcde\", \"abcde\" " << std::endl
+            << "Output: " << BackSpaceStringCompare(test1[0], test1[1]) << std::endl
+            << std::endl;
+  std::cout << "Input strings 2 with deletes and spaces: \"Uber Career Prep\", \"u#Uber Careee#r Prep\" " << std::endl
+            << "Output: " << BackSpaceStringCompare(test2[0], test2[1]) << std::endl
+            << std::endl;
+  std::cout << "Input strings 3 with deletes and a match: \"abcdef###xyz\", \"abcw#xyz\" " << std::endl
+            << "Output: " << BackSpaceStringCompare(test3[0], test3[1]) << std::endl
+            << std::endl;
+  std::cout << "Input strings 4 with no match: \"abcdef###xyz\",\"abcdefxyz###\" " << std::endl
+            << "Output: " << BackSpaceStringCompare(test4[0], test4[1]) << std::endl
+            << std::endl;
 }
 
-//Time completed in: 35 minutes
+// Time completed in: 35 minutes
--- Assignment-1/KAnagrams.cpp
+++ Assignment-1/KAnagrams.cpp
@@ -1,58 +1,68 @@
-//Hash the elements technique (using an array)
-//Space is O(1), runtime is O(n) 
+// Hash the elements technique (using an array)
+// Space is O(1), runtime is O(n)
 
-
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <bits/stdc++.h>
 
+std::string KAnagrams(std::string firstString, std::string secondString, int k)
+{
+    int letters[128] = {0};
 
+    if (firstString.length() != secondString.length())
+        return "false";
 
-std::string KAnagrams(std::string in1, std::string in2, int k){
-int letters[128]={0};
+    for (int i = 0; i < firstString.length(); i++)
+    {
+        letters[int(firstString[i])]++;
+        letters[int(secondString[i])]--;
+    }
+    int diff = 0;
+    for (int i = 0; i < 128; i++)
+    {
+        if (letters[i] != 0)
+            diff++;
+    }
+    diff = diff / 2;
 
-if(in1.length()!=in2.length())return "false";
+    if (diff > k)
+        return "false";
 
-for(int i=0;i<in1.length();i++){
-letters[int(in1[i])]++;
-letters[int(in2[i])]--;
-}
-int diff=0;
-for(int i=0;i<128;i++){
-if(letters[i]!=0)diff++;
-}
-diff=diff/2;
-
-if(diff>k)return "false";
-
-return "true";
-
-}
-
-
-int main(){
-
-std::string test1[2]={"apple", "peach"};
-int k1=1;
-int k2=2;
-
-std::string test3[2]={ "cat", "dog"};
-int k3=3;
-
-std::string test4[2]={"debit curd", "bad credit"};
-int k4=1;
-
-std::string test5[2]={"baseball", "basketball"};
-int k5=2;
-
-
-std::cout << std::boolalpha;  
-std::cout<<"Input Strings: \"apple\", \"peach\" "<<std::endl<<"Input k: 1"<<std::endl<<KAnagrams(test1[0],test1[1],k1)<<"\n\n";
-std::cout<<"Input Strings: \"apple\", \"peach\" "<<std::endl<<"Input k: 2"<<std::endl<<KAnagrams(test1[0],test1[1],k2)<<"\n\n";
-std::cout<<"Input Strings: \"cat\", \"dog\" "<<std::endl<<"Input k: 3"<<std::endl<<KAnagrams(test3[0],test3[1],k3)<<"\n\n";
-std::cout<<"Input Strings: \"debit curd\", \"bad credit\" "<<std::endl<<"Input k: 1"<<std::endl<<KAnagrams(test4[0],test4[1],k4)<<"\n\n";
-std::cout<<"Input Strings: \"baseball\", \"basketball\" "<<std::endl<<"Input k: 2"<<std::endl<<KAnagrams(test5[0],test5[1],k5)<<"\n\n";
-
+    return "true";
 }
 
-//Time completed in: 23 minutes
+int main()
+{
+
+    std::string test1[2] = {"apple", "peach"};
+    int k1 = 1;
+    int k2 = 2;
+
+    std::string test3[2] = {"cat", "dog"};
+    int k3 = 3;
+
+    std::string test4[2] = {"debit curd", "bad credit"};
+    int k4 = 1;
+
+    std::string test5[2] = {"baseball", "basketball"};
+    int k5 = 2;
+
+    std::cout << std::boolalpha;
+    std::cout << "Input Strings that need more than 1 change (invalid): \"apple\", \"peach\" " << std::endl
+              << "Input k: 1" << std::endl
+              << KAnagrams(test1[0], test1[1], k1) << "\n\n";
+    std::cout << "Input Strings that need 2 changes (valid): \"apple\", \"peach\" " << std::endl
+              << "Input k: 2" << std::endl
+              << KAnagrams(test1[0], test1[1], k2) << "\n\n";
+    std::cout << "Input Strings where K is the same as length: \"cat\", \"dog\" " << std::endl
+              << "Input k: 3" << std::endl
+              << KAnagrams(test3[0], test3[1], k3) << "\n\n";
+    std::cout << "Input Strings with mutiple words (valid): \"debit curd\", \"bad credit\" " << std::endl
+              << "Input k: 1" << std::endl
+              << KAnagrams(test4[0], test4[1], k4) << "\n\n";
+    std::cout << "Input Strings with different lengths: \"baseball\", \"basketball\" " << std::endl
+              << "Input k: 2" << std::endl
+              << KAnagrams(test5[0], test5[1], k5) << "\n\n";
+}
+
+// Time completed in: 23 minutes
