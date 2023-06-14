0bb01f81bbfd460d0b7589a30083e0ce25beea4d
Ahmed Fawzy <ahmed_ehab03@outlook.com>
2023-06-13 23:23:07 -0400

    fixed sol

--- Assignment-3/Q8AlternatingPath.cpp
+++ Assignment-3/Q8AlternatingPath.cpp
@@ -8,81 +8,75 @@ Time complexity:  O(V+E)
 Space complexity: O(V)
 */
 
-
 #include <iostream>
 #include <vector>
 #include <stack>
 
-
-
-struct node{
+struct node
+{
     std::string value = "";
     bool visited = false;
     bool prevIsRed = false;
 
-    std::vector<node*> edges = {};
+    std::vector<node *> edges = {};
     std::vector<bool> edgeIsRed = {};
 
-
-    node(std::string a){
+    node(std::string a)
+    {
         value = a;
     }
 };
 
-
-
-int AlternatingPath(std::vector<node*> nodes, node* start, node* end)
+int AlternatingPath(std::vector<node *> nodes, node *start, node *end)
 {
-    
-    std::stack<std::pair<node*, int>> order;
+
+    std::stack<std::pair<node *, int>> order;
 
     order.push({start, 0});
- 
-    while(!order.empty()){
 
-        std::pair<node*, int> cur = order.top();
+    while (!order.empty())
+    {
+
+        std::pair<node *, int> cur = order.top();
         order.pop();
 
         bool edgePrevIsRed = cur.first->prevIsRed;
         cur.first->visited = true;
 
-        if (cur.first == end){
+        if (cur.first == end)
+        {
             return cur.second;
         }
 
-        for(int i=0; i<cur.first->edges.size(); i++){
+        for (int i = 0; i < cur.first->edges.size(); i++)
+        {
 
-            bool alternateEdges = (edgePrevIsRed^cur.first->edgeIsRed[i]) ||cur.first == start;
+            bool alternateEdges = (edgePrevIsRed ^ cur.first->edgeIsRed[i]) || cur.first == start;
 
-            if(!cur.first->edges[i]->visited && alternateEdges){
-                
+            if (!cur.first->edges[i]->visited && alternateEdges)
+            {
+
                 cur.first->edges[i]->prevIsRed = cur.first->edgeIsRed[i];
-                order.push({cur.first->edges[i],cur.second+1});
+                order.push({cur.first->edges[i], cur.second + 1});
             }
-
         }
-
     }
 
     return -1;
-   
 }
 
-
-
 int main()
-{   
+{
 
-
-    //TEST 1: testing with a medium sized graph, containing cycles and a valid path between start and end
+    // TEST 1: testing with a medium sized graph, containing cycles and a valid path between start and end
     node A("A");
     node B("B");
     node C("C");
     node D("D");
     node E("E");
-    std::vector<node*> nodes = {&A, &B, &C, &D, &E};
+    std::vector<node *> nodes = {&A, &B, &C, &D, &E};
 
-    A.edges = {&B, &C, &D}; 
+    A.edges = {&B, &C, &D};
     A.edgeIsRed = {false, true, true};
 
     B.edges = {&D, &E};
@@ -96,10 +90,9 @@ int main()
 
     E.edges = {&C};
     E.edgeIsRed = {true};
-    
-    std::cout<<"Testing with graph #1 from A to E: "<<AlternatingPath(nodes, &A, &E)<<"\n";
 
-    //TEST 2: Same graph as test 1, but testing two nodes with no valid path between them
-    std::cout<<"Testing with graph #1 from A to E: "<<AlternatingPath(nodes, &E, &D)<<"\n";
+    std::cout << "Testing with graph #1 from A to E: " << AlternatingPath(nodes, &A, &E) << "\n";
 
+    // TEST 2: Same graph as test 1, but testing two nodes with no valid path between them
+    std::cout << "Testing with graph #1 from A to E: " << AlternatingPath(nodes, &E, &D) << "\n";
 }
---
+++ Assignment-3/Q9MergeKSortedArrays.cpp
@@ -0,0 +1,84 @@
+/*
+n is total number of elements
+
+Time spent:       35 minutes
+Technique:        Min priority Queue (put first element of each array into pq, pop, push first element in the array that the popped value came from..)
+Time complexity:  O(nlogk)
+Space complexity: O(n)
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+auto comparator = [](const std::pair<int, int> &a, const std::pair<int, int> &b)
+{
+    return a.first > b.first;
+};
+
+std::vector<int> MergeKSortedArrays(std::vector<std::vector<int>> &arrays)
+{
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, decltype(comparator)> pq(comparator); // min pq
+
+    std::vector<int> arrIndex = {};
+    std::vector<int> finalArr = {};
+
+    for (int k = 0; k < arrays.size(); k++)
+    {
+
+        pq.push({arrays[k][0], k});
+        arrIndex.push_back(1);
+    }
+
+    while (!pq.empty())
+    {
+        std::pair<int, int> top = pq.top();
+        pq.pop();
+
+        finalArr.push_back(top.first);
+
+        int k = top.second;
+        if (arrIndex[k] <= arrays[k].size() - 1)
+        {
+
+            pq.push({arrays[k][arrIndex[k]], k});
+            arrIndex[k]++;
+        }
+    }
+
+    return finalArr;
+}
+
+void printArr(std::vector<int> arr)
+{
+
+    std::cout << "[";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i];
+
+        if (i != arr.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]\n\n";
+}
+
+int main()
+{
+
+    // TEST 1: testing with 2 arrays that have some overlapping intervals
+    std::vector<std::vector<int>> input1 = {{1, 2, 3, 4, 5}, {1, 3, 5, 7, 9}};
+    std::cout << "Merging the following arrays: [1, 2, 3, 4, 5], [1, 3, 5, 7, 9]\n";
+    std::cout << "Output: ";
+    printArr(MergeKSortedArrays(input1));
+
+    // TEST 2: testing with 3 bigger arrays that have many overlapping intervals and even duplicate numbers
+    std::vector<std::vector<int>> input2 = {{1, 4, 7, 9}, {2, 6, 7, 10, 11, 13, 15}, {3, 8, 12, 13, 16}};
+    std::cout << "Merging the following arrays: [1, 4, 7, 9], [2, 6, 7, 10, 11, 13, 15], [3, 8, 12, 13, 16]\n";
+    std::cout << "Output: ";
+    printArr(MergeKSortedArrays(input2));
+
+    return 0;
+}
