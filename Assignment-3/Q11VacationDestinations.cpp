efb22886b10ebec9f56970b1ab9faa1c7dfc7fe7
Ahmed Fawzy <ahmed_ehab03@outlook.com>
2023-06-17 03:54:27 -0400

    fix

--- Assignment-3/Q11VacationDestinations.cpp
+++ Assignment-3/Q11VacationDestinations.cpp
@@ -5,13 +5,12 @@ E is number of edges
 Time spent:       40 minutes
 Technique:        DFS while keeping track of shortest distance in each node
 Time complexity:  O(V*E)
-Space complexity: O(V*E) including recusrive stack
+Space complexity: O(V*E) including recursive stack
 */
 
 #include <iostream>
 #include <vector>
 #include <map>
-
 
 std::map<std::string, std::vector<std::pair<std::string, double>>> adjacencySet(std::vector<std::pair<std::string, std::string>> edges, std::vector<double> weights)
 {
@@ -28,21 +27,22 @@ std::map<std::string, std::vector<std::pair<std::string, double>>> adjacencySet(
     return adjacencyMap;
 }
 
-void dfs(std::string node, int k, std::map<std::string, std::vector<std::pair<std::string, double>>>& graph, std::map<std::string, double>& shortestPath){
+void dfs(std::string node, int k, std::map<std::string, std::vector<std::pair<std::string, double>>> &graph, std::map<std::string, double> &shortestPath)
+{
 
-  for(int i=0; i<graph[node].size(); i++){
+    for (int i = 0; i < graph[node].size(); i++)
+    {
 
         bool option1 = !shortestPath.count(graph[node][i].first) && graph[node][i].second + shortestPath[node] <= k;
         bool option2 = shortestPath.count(graph[node][i].first) && shortestPath[graph[node][i].first] > shortestPath[node] + graph[node][i].second;
 
-        if(option1 || option2){
+        if (option1 || option2)
+        {
 
             shortestPath[graph[node][i].first] = graph[node][i].second + shortestPath[node] + 1;
             dfs(graph[node][i].first, k, graph, shortestPath);
         }
-  }
-
-    
+    }
 }
 
 int VacationDestinations(std::vector<std::pair<std::string, std::string>> edges, std::vector<double> weights, std::string origin, int k)
@@ -51,30 +51,24 @@ int VacationDestinations(std::vector<std::pair<std::string, std::string>> edges,
     std::map<std::string, double> shortestPath;
     shortestPath[origin] = 0;
     dfs(origin, k, graph, shortestPath);
-    
 
-    return shortestPath.size()-1;
-    
+    return shortestPath.size() - 1;
 }
-
 
 int main()
 {
     std::vector<std::pair<std::string, std::string>> edges1 = {{"Boston", "New York"}, {"New York", "Philadelphia"}, {"Boston", "Newport"}, {"Washington, D.C.", "Harper's Ferry"}, {"Boston", "Portland"}, {"Philadelphia", "Washington, D.C."}};
     std::vector<double> weights1 = {4, 2, 1.5, 1, 2.5, 2.5};
-    
-    //TEST 1: Checking for k=5, should not count some cities due to the 1 hour layover, total 2 cities.
-    std::cout<<"Testing the first graph with origin at New York and K = 5: \n";
-    std::cout<<"Output: "<<VacationDestinations(edges1, weights1, "New York", 5)<<" different cities\n";
 
-    //TEST 2: Checking for k=7, should count more cities that would be exclucded in k=5 for a total of 4.
-    std::cout<<"Testing the first graph with origin at New York and K = 7: \n";
-    std::cout<<"Output: "<<VacationDestinations(edges1, weights1, "New York", 7)<<" different cities\n";
+    // TEST 1: Checking for k=5, should not count some cities due to the 1 hour layover, total 2 cities.
+    std::cout << "Testing the first graph with origin at New York and K = 5: \n";
+    std::cout << "Output: " << VacationDestinations(edges1, weights1, "New York", 5) << " different cities\n";
 
-    //TEST 3: Checking for k=8, even though 1 more distance, should allow 2 more cities for a total of 6.
-    std::cout<<"Testing the first graph with origin at New York and K = 8: \n";
-    std::cout<<"Output: "<<VacationDestinations(edges1, weights1, "New York", 8)<<" different cities\n";
-    
+    // TEST 2: Checking for k=7, should count more cities that would be exclucded in k=5 for a total of 4.
+    std::cout << "Testing the first graph with origin at New York and K = 7: \n";
+    std::cout << "Output: " << VacationDestinations(edges1, weights1, "New York", 7) << " different cities\n";
 
-   
+    // TEST 3: Checking for k=8, even though 1 more distance, should allow 2 more cities for a total of 6.
+    std::cout << "Testing the first graph with origin at New York and K = 8: \n";
+    std::cout << "Output: " << VacationDestinations(edges1, weights1, "New York", 8) << " different cities\n";
 }
